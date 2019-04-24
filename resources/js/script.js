1a6fe7983bb932f6b61d073333850ff5e2a97442
nikos.skl <nikos.skl98@gmail.com>
2019-04-24 12:34:14 +0300

    First commit

---
+++ resources/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenavId").style.width = "250px";
+    console.log('hiii');
+}
+
+function closeNav() {
+    document.getElementById("sidenavId").style.width = "0px";
+    
+}
